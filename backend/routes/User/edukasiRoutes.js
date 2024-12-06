86cda90ba240c1c6721deeb08a9dfa24ce425dd4
RaihanPrasetia <raihanprasetia86@gmail.com>
2024-12-07 00:09:59 +0700

    fix bug home

--- backend/routes/User/edukasiRoutes.js
+++ backend/routes/User/edukasiRoutes.js
@@ -5,7 +5,7 @@ const authenticateJWT = require('../../middlewares/authMiddleware');
 const { upload, createEducation, deleteEducation, getEducationById, getEducations, updateEducation } = require('../../controller/User/edukasiController')
 
 // Mitra
-router.get('/educations', upload.none(), authenticateJWT, getEducations);
+router.get('/educations', upload.none(), getEducations);
 router.get('/educations/:id', upload.none(), authenticateJWT, getEducationById);
 router.post('/educations/create', upload.single('image_path'), authenticateJWT, createEducation);
 router.put('/educations/update/:id', upload.single('image_path'), authenticateJWT, updateEducation);
